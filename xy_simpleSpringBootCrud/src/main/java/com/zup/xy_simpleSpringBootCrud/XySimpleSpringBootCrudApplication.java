b0bb21f73b4955cbb64836b4afdf238c853d4515
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-11-09 15:01:01 -0200

    firtCommit

---
+++ xy_simpleSpringBootCrud/src/main/java/com/zup/xy_simpleSpringBootCrud/XySimpleSpringBootCrudApplication.java
@@ -0,0 +1,12 @@
+package com.zup.xy_simpleSpringBootCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XySimpleSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XySimpleSpringBootCrudApplication.class, args);
+	}
+}
